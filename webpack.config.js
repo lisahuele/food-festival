1983e6f1a4199fa5d55bfb9d1c65bc7fc016e5c1
lisahuele <lisahuele@gmail.com>
2021-09-09 15:10:14 -0400

    optimized images

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,49 @@
 const path = require('path');
 const webpack  = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: { 
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        tickets: './assets/js/tickets.js',
+        schedule: './assets/js/tickets.js'
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    { 
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name (file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery'
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", //report outputs an HTML file in dist folder
         })
     ],
     mode: 'development'
